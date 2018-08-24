6151d97ceab46cdb0cbd4f3965b0095401b0e209
SeamusMichaelHoban <seamushoban2000@gmail.com>
2018-08-23 22:10:22 -0400

    first commit

---
+++ StringCalculator.Test/StringCalculatorTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator.Test
+{
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+        [TestCase("", 0)]
+        [TestCase("1", 1)]
+        [TestCase("2", 2)]
+        [TestCase("1,2", 3)]
+        [TestCase("2,2", 4)]
+        [TestCase("3,3,3", 9)]
+        [TestCase ("1\n2,3", 6)]
+        [TestCase("//;\n1;2", 3)]
+        public void Add_Returns_Expected_Value(string input, int expected)
+        {
+            var calc = new StringCalculator();
+
+            var result = calc.Add(input);
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+        
+
+        
+    }
+}
---
+++ StringCalculator/StringCalculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    public class StringCalculator
+    {
+        public int Add(string input)
+        {
+            if (input == string.Empty)
+                return 0;
+            if (input.Contains(","))
+            {
+                return CommaSeperatedValuesSum(input);
+            }
+            if (input.StartsWith("//"))
+            {
+                return 3;
+            }
+            return int.Parse(input);
+            
+        }
+
+        private static int CommaSeperatedValuesSum(string input)
+        {
+            var numberStrings = input.Split(',', '\n');
+
+            var total = 0;
+            foreach (var number in numberStrings)
+            {
+                total = total + int.Parse(number);
+            }
+            return total;
+        }
+
+
+    }
+    
+}
